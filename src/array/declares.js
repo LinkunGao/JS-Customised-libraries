5d77592bb7c31ab38d97cd2f569d890b12acd688
LinkunGao <gaolinkun123@gmail.com>
2021-12-23 14:54:26 +1300

    finish filter

--- src/array/declares.js
+++ src/array/declares.js
@@ -27,3 +27,23 @@ function reduce(arr, callback, initValue) {
   //   返回最终的结果
   return result;
 }
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function filter(arr, callback) {
+  // 声明一个空数组
+  let result = [];
+  // 遍历数组
+  for (let i = 0; i < arr.length; i++) {
+    //执行回调
+    let res = callback(arr[i], i);
+    // 判断
+    if (res) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
