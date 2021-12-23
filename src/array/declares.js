d6ecee11e52e547211524b694a132284ecc1f6d2
LinkunGao <gaolinkun123@gmail.com>
2021-12-23 20:41:56 +1300

    findIndex,every,some

--- src/array/declares.js
+++ src/array/declares.js
@@ -47,3 +47,63 @@ function filter(arr, callback) {
   }
   return result;
 }
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function find(arr, callback) {
+  // 遍历数组
+  for (let i = 0; i < arr.length; i++) {
+    let res = callback(arr[i], i);
+    if (res) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function findIndex(arr, callback) {
+  // 遍历数组
+  for (let i = 0; i < arr.length; i++) {
+    let res = callback(arr[i], i);
+    if (res) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function every(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i], i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ */
+function some(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i)) {
+      return true;
+    }
+  }
+  return false;
+}
