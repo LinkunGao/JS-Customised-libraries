1f8929cb924ddcf9278da939b3e6af0819e7259b
LinkunGao <gaolinkun123@gmail.com>
2021-12-23 22:21:45 +1300

    unique array

--- src/array/declares.js
+++ src/array/declares.js
@@ -107,3 +107,38 @@ function some(arr, callback) {
   }
   return false;
 }
+
+function unique(arr) {
+  const result = [];
+  arr.forEach((item) => {
+    // 检测result数组中是否有这个值
+    if (result.indexOf(item) === -1) {
+      //若没有该元素，则插入到数组中
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+function unique2(arr) {
+  // 声明空数组
+  const result = [];
+  // 声明空对象
+  const obj = {};
+  arr.forEach((item) => {
+    if (obj[item] === undefined) {
+      //   将item作为下标存储在obj中
+      obj[item] = true;
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+function unique3(arr) {
+  // 将数组转化set集合
+  let set = new Set(arr);
+  // 将set展开创建数组
+  let array = [...set];
+  return array;
+}
