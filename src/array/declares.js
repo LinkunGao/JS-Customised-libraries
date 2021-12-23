4312ff85c3a6a40df03710509e1930c9a9b617f9
LinkunGao <gaolinkun123@gmail.com>
2021-12-23 14:17:40 +1300

    finish map and reduce

---
+++ src/array/declares.js
@@ -0,0 +1,29 @@
+function map(arr, callback) {
+  // 声明一个空的数组
+  let result = [];
+  // 遍历数组
+  for (let i = 0; i < arr.length; i++) {
+    // 执行回调
+    result.push(callback(arr[i], i));
+  }
+  return result;
+}
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+ * @param {*} initValue
+ * @returns
+ */
+function reduce(arr, callback, initValue) {
+  // 声明变量
+  let result = initValue;
+  // 执行回调
+  for (let i = 0; i < arr.length; i++) {
+    // 执行回调
+    result = callback(result, arr[i]);
+  }
+  //   返回最终的结果
+  return result;
+}
